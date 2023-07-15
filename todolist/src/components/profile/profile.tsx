d2e0f20e662e7a7a990029e5e7e109439aed4b48
Michał Furgała <mchfrg@gmail.com>
2023-07-15 12:37:06 +0200

    InitProject&FirstComponents

---
+++ todolist/src/components/profile/profile.tsx
@@ -0,0 +1,7 @@
+import React, { FC, ReactElement } from 'react';
+import {Avatar,Box,Typography} from '@mui/material';
+
+export const Profile: FC = (): ReactElement => {
+
+    return <h2>Profile</h2>
+}
